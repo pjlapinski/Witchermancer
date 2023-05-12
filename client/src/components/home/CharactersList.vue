207e96516d08dd7dfe798ad4aba28a8746fd0aa2
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2023-05-12 02:04:23 +0200

    Home page when signed out

---
+++ client/src/components/home/CharactersList.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+| characters list
+</template>
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import type { Character } from '@/domain/types/character'
+
+const props = defineProps<{ characters: Character[] }>()
+</script>
--- client/src/components/pages/HomePage.vue
+++ client/src/components/pages/HomePage.vue
@@ -1,17 +1,20 @@
 ﻿<template lang="pug">
-template(v-if='user.isAuthenticated')
-  div(v-for='char in characters', v-if='characters.length > 0')
-    | {{ char.name }}
-  div(v-else) No characters!
-template(v-else)
-  div login!
+main#home-page-content(v-if='user.isAuthenticated')
+  button.btn-subtle {{ $t('home.createCharacter') }}
+  characters-list(:characters='characters')
+main#home-page-content(v-else)
+  h1 {{ $t('home.greeting') }}
+  p#website-description {{ $t('home.description') }}
+  #split-line
+  button#sign-in-btn(@click='handleSignInBtnClick()') {{ $t('home.signIn') }}
 </template>
 
 <script setup lang="ts">
+import CharactersList from '@/components/home/CharactersList.vue'
 import { reactive } from 'vue'
-import { Character } from '@/domain/types/character'
 import { getUserCharacters } from '@/domain/api'
 import { useUserStore } from '@/domain/store/user'
+import type { Character } from '@/domain/types/character.d.ts'
 
 let user = useUserStore()
 
@@ -19,4 +22,31 @@ let characters = reactive<Character[]>([])
 
 if (user.isAuthenticated)
   getUserCharacters().then(chars => (characters = chars))
+
+const handleSignInBtnClick = () => {
+  window.location.pathname = '/api/login'
+}
 </script>
+
+<style scoped lang="scss">
+#home-page-content {
+  @extend .d-flex, .flex-col, .text-center, .p-3, .mt-5;
+
+  gap: 5vh;
+}
+
+#website-description {
+  @extend .h3;
+}
+
+#split-line {
+  border-top: 1px solid var(--color-5);
+}
+
+#sign-in-btn {
+  @extend .btn-subtle, .mx-auto, .py-1, .px-3, .h3;
+
+  width: fit-content;
+  color: var(--color-5);
+}
+</style>
