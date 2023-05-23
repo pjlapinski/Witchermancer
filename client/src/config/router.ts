845a29c16b12bad710592c76bc92c1d5c220589d
Przemysław Łapiński <przemyslaw.j.lapinski@gmail.com>
2023-05-24 01:03:44 +0200

    Vue error pages redirect to home

--- client/src/config/router.ts
+++ client/src/config/router.ts
@@ -19,6 +19,12 @@ const routes: RouteRecordRaw[] = [
     name: 'CharacterSheet',
     component: CharacterSheet,
   },
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: {
+      name: 'Home',
+    },
+  },
 ]
 
 export default createRouter({
